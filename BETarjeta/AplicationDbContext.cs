8cc12bc854dcc0d892e05fae5952a81d63f71bc1
Noel Ortiz <20175580@itla.edu.do>
2021-12-15 12:35:14 -0400

    Agregar archivos de proyecto.

---
+++ BETarjeta/AplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using BETarjeta.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace BETarjeta
+{
+    public class AplicationDbContext: DbContext
+
+    {
+      
+        DbSet<TarjetaCredito>  TarjetaCredito { get; set; }
+
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options): base(options)
+        {
+
+        }
+    }
+}
---
+++ BETarjeta/Models/TarjetaCredito.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading.Channels;
+
+
+namespace BETarjeta.Models
+
+{
+    public class TarjetaCredito
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        public string Titular { get; set; }
+        [Required]
+        public string NumeroTarjeta { get; set; }
+        [Required]
+        public string FechaExpiracaion { get; set; }
+        [Required]
+        public string CVV { get; set; }
+    }
+}
